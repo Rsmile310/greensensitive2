aba97325b84c5953d4700af0651095bcfe02bd29
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-05-26 16:39:50 +0900

    update

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,7 +9,7 @@ import { Box } from '@mui/material';
 const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
   <>
     <Helmet>
-      <title>{`${title} | Greansensitive`}</title>
+      <title>{`${title} | Greensensitive`}</title>
       {meta}
     </Helmet>
 
